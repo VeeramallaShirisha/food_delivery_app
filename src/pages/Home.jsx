acc6f3d22e9b78de8c111151f7facb1e95b21557
VeeramallaShirisha <137159707+VeeramallaShirisha@users.noreply.github.com>
2025-03-24 22:57:03 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 align="center">Welcome to the Food Delivery App</h1>
+      <img src="./img.jpeg" alt="" style={{ height: '550px', width: '1550px' }}/>
+    </div>
+  )
+}
+
+export default Home
